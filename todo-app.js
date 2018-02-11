a14cacb5bb10d6908d24ace1854a4733120aefeb
Sanghee Lee <sanghee911@hotmail.com>
2018-02-12 01:44:37 +0900

    removed todo.vue

--- todo-app.js
+++ todo-app.js
@@ -11,7 +11,7 @@ app.use('/src', express.static('src'));
 app.use('/dist', express.static('dist'));
 
 if(typeof process.env.TODO_BACKEND_URL === 'undefined') {
-  global.todoBackendUrl = 'http://localhost:8000/api/todo-list/'
+  global.todoBackendUrl = 'http://minikube:32002/api/todo-list/'
 } else {
   global.todoBackendUrl = process.env.TODO_BACKEND_URL;
 }
